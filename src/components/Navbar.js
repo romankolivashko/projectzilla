e49c01b9df54b54c2f8f954e184571ddcc78650e
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-30 22:48:16 -0800

    Added sidebar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom'
 import './Navbar.css'
-import Logo from '../assets/logo2.svg'
+import Logo from '../assets/logo3.svg'
 
 export default function Navbar() {
   return (
