d0c240da5c761553ac948ce0cc34a6d74afeef2a
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-30 16:59:44 -0800

    Created firebase config file

---
+++ src/firebase/config.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDnYzoNh7LTpdHfLLi4xZx3d5hzoUBV8Q8",
+  authDomain: "projectzilla-5192f.firebaseapp.com",
+  projectId: "projectzilla-5192f",
+  storageBucket: "projectzilla-5192f.appspot.com",
+  messagingSenderId: "922298661826",
+  appId: "1:922298661826:web:8e9af4c43d53472f82b45b"
+};
+
+// init firebase
+firebase.initializeApp(firebaseConfig)
+
+//init services
+const projectFirestore = firebase.firestore()
+const projectAuth = firebase.auth()
+
+// timestamp
+const timestamp = firebase.firestore.Timestamp
+
+export { projectFirestore, projectAuth, timestamp }
