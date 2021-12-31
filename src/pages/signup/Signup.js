e0ba48b0bae7e9d0b69dc674d4825ef1f286eee1
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-30 23:22:46 -0800

    Added signup form

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -1,9 +1,58 @@
 import './Signup.css'
+import { useState } from 'react'
 
 export default function Signup() {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [displayName, setDisplayName] = useState('')
+  const [thumbnail, setThumbnail] = useState('')
+
   return (
-    <div>
-      Signup
-    </div>
+    <form className='auth'>
+      <h2>Sign up</h2>
+      <label>
+        <span>
+          email:
+        </span>
+        <input 
+          required
+          type="email"
+          onChange={(e) => setEmail(e.target.value)}
+          value={email}
+        />
+      </label>
+      <label>
+        <span>
+          password:
+        </span>
+        <input 
+          required
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+          value={password}
+        />
+      </label>
+      <label>
+        <span>
+          display name:
+        </span>
+        <input 
+          required
+          type="text"
+          onChange={(e) => setDisplayName(e.target.value)}
+          value={displayName}
+        />
+      </label>
+      <label>
+        <span>
+          profile thumbnail:
+        </span>
+        <input 
+          required
+          type="file"
+        />
+      </label>
+      <button>Sign up</button>
+    </form>
   )
 }
