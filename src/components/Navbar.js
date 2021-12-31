066e40ce52607bc59032c2959dd8d73d54d4a17a
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-31 14:20:12 -0800

    Added condition to navbar links to be visible

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,9 +2,11 @@ import { Link } from 'react-router-dom'
 import './Navbar.css'
 import Logo from '../assets/logo3.svg'
 import { useLogout } from '../hooks/useLogout'
+import { useAuthContext } from '../hooks/useAuthContext';
 
 export default function Navbar() {
   const { logout, isPending } = useLogout()
+  const { user } = useAuthContext();
 
   return (
     <div className="navbar">
@@ -13,13 +15,21 @@ export default function Navbar() {
           <img src={Logo} alt="dino logo" />
           <span>The Project - zilla</span>
         </li>
-        <li><Link to="/login">Login</Link></li>
-        <li><Link to="/signup">Signup</Link></li>
-        <li>
-         { !isPending && <button className="btn" onClick={logout}>Logout</button> }
-         { isPending && <button className="btn" onClick={logout}>Logging...</button> }
-        </li>
-      </ul>
+        {!user && (
+          <>
+        <li><Link to="/login">Login</Link> </li>
+        <li><Link to="/signup">Signup</Link> </li>
+          </>
+        )}
+
+        {user && (
+          <li>
+          { !isPending && <button className="btn" onClick={logout}>Logout</button> }
+          { isPending && <button className="btn" onClick={logout}>Logging...</button> }
+          </li>
+        )}
+        </ul>
+        
     </div>
   )
 }
