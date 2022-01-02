fd4766c5cc295c6d362ca1bd86e3d317abda581f
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-02 13:49:12 -0800

    Added projectSummary pages + styles

--- src/pages/project/Project.js
+++ src/pages/project/Project.js
@@ -1,6 +1,7 @@
 import { useParams } from 'react-router-dom'
 import { useDocument } from '../../hooks/useDocument'
 import './Project.css'
+import ProjectSummary from './ProjectSummary'
 
 export default function Project() {
   const { id } = useParams()
@@ -18,7 +19,7 @@ export default function Project() {
 
   return (
     <div className='project-details'>
-      <h1>{document.name}</h1>
+      <ProjectSummary project={document} />
     </div>
   )
 }
