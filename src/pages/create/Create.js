f67054975440b24866bb149f0ac412cc5805f0fe
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-01 19:21:10 -0800

    Added saving the project functionality

--- src/pages/create/Create.js
+++ src/pages/create/Create.js
@@ -1,130 +1,137 @@
-import './Create.css'
-import { useEffect, useState } from 'react';
-import Select from 'react-select'
-import { useCollection } from '../../hooks/useCollection'
-import { timestamp } from '../../firebase/config';
-import { useAuthContext } from '../../hooks/useAuthContext'
+import "./Create.css";
+import { useEffect, useState } from "react";
+import Select from "react-select";
+import { useCollection } from "../../hooks/useCollection";
+import { timestamp } from "../../firebase/config";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useHistory } from "react-router-dom";
+import { useFirestore } from "../../hooks/useFirestore";
 
 const categories = [
-  { value: 'development', label: 'Development'},
-  { value: 'design', label: 'Design'},
-  { value: 'sales', label: 'Sales'},
-  { value: 'marketing', label: 'Marketing'},
-]
+  { value: "development", label: "Development" },
+  { value: "design", label: "Design" },
+  { value: "sales", label: "Sales" },
+  { value: "marketing", label: "Marketing" },
+];
 
 export default function Create() {
-  const { documents } = useCollection('users')
-  const [users, setUsers] = useState([])
-  const { user } = useAuthContext()
+  const history = useHistory()
+  const { addDocument, response } = useFirestore('projects');
+  const { documents } = useCollection("users");
+  const [users, setUsers] = useState([]);
+  const { user } = useAuthContext();
 
   //form fiell values
-  const [name, setName] = useState('');
-  const [details, setDetails] = useState('');
-  const [dueDate, setDueDate] = useState('');
-  const [category, setCategory] = useState('');
+  const [name, setName] = useState("");
+  const [details, setDetails] = useState("");
+  const [dueDate, setDueDate] = useState("");
+  const [category, setCategory] = useState("");
   const [assignedUsers, setAssignedUsers] = useState([]);
-  const [formError, setFormError] = useState(null)
+  const [formError, setFormError] = useState(null);
 
-useEffect(() => {
-  if(documents){
-    const options = documents.map(user => {
-      return { value: user, label: user.displayName}
-    })
-    setUsers(options)
-  }
-}, [documents])
+  useEffect(() => {
+    if (documents) {
+      const options = documents.map((user) => {
+        return { value: user, label: user.displayName };
+      });
+      setUsers(options);
+    }
+  }, [documents]);
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    setFormError(null)
-    
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setFormError(null);
+
     if (!category) {
-      setFormError('Please select a project category')
-      return
+      setFormError("Please select a project category");
+      return;
     }
 
     if (assignedUsers.length < 1) {
-      setFormError('Please assign the project to at least 1 user')
-      return
+      setFormError("Please assign the project to at least 1 user");
+      return;
     }
-  }
+    
+    const createdBy = {
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+      id: user.id,
+    };
+    
+    const assignedUsersList = assignedUsers.map((u) => {
+      return {
+        displayName: u.value.displayName,
+        photoURL: u.value.photoURL,
+        id: u.value.id,
+      };
+    });
+    
+    const project = {
+      name,
+      details,
+      category: category.value,
+      dueDate: timestamp.fromDate(new Date(dueDate)),
+      comments: [],
+      createdBy,
+      assignedUsersList,
+    };
 
-  const createdBy = {
-    displayName: user.displayName,
-    photoURL: user.photoURL,
-    id: user.id
-  }
-
-  const assignedUsersList = assignedUsers.map((u) =>{
-    return {
-      displayName: u.value.displayName,
-      photoURL: u.value.photoURL,
-      id: u.value.id
+    await addDocument(project)
+    if (!response.error){
+      history.push('/')
     }
-  })
-
-  const project = {
-    name,
-    details,
-    category: category.value,
-    dueDate: timestamp.fromDate(new Date(dueDate)),
-    comments: [],
-    createdBy,
-    assignedUsersList
-  }
-
-  console.log(project);
+  };
 
   return (
-    <div className='create-form'>
-      <h2 className='page-title'>Create a new project</h2>
+    <div className="create-form">
+      <h2 className="page-title">Create a new project</h2>
       <form onSubmit={handleSubmit}>
-      <label>
-        <span>Project name:</span>
-        <input 
-          required
-          type="text"
-          onChange={(e) => setName(e.target.value)}
-          vaule={name}
-        />
-      </label>
-      <label>
-        <span>Project details:</span>
-        <textarea 
-          required
-          type="text"
-          onChange={(e) => setDetails(e.target.value)}
-          vaule={name}
-        ></textarea>
-      </label>
-      <label>
-        <span>Set due date:</span>
-        <input 
-          required
-          type="date"
-          onChange={(e) => setDueDate(e.target.value)}
-          vaule={dueDate}
-        />
-      </label>
-      <label>
-        <span>Project Category:</span>
-        <Select 
-          onChange={(option) => setCategory(option)}
-          options={categories}
-        />
-      </label>
-      <label>
-        <span>Assign to:</span>
-        <Select 
-          onChange={(option) => setAssignedUsers(option)}
-          options={users}
-          isMulti
-        />
-      </label>
-      <button className='btn'>Add Project</button>
+        <label>
+          <span>Project name:</span>
+          <input
+            required
+            type="text"
+            onChange={(e) => setName(e.target.value)}
+            vaule={name}
+          />
+        </label>
+        <label>
+          <span>Project details:</span>
+          <textarea
+            required
+            type="text"
+            onChange={(e) => setDetails(e.target.value)}
+            vaule={name}
+          ></textarea>
+        </label>
+        <label>
+          <span>Set due date:</span>
+          <input
+            required
+            type="date"
+            onChange={(e) => setDueDate(e.target.value)}
+            vaule={dueDate}
+          />
+        </label>
+        <label>
+          <span>Project Category:</span>
+          <Select
+            onChange={(option) => setCategory(option)}
+            options={categories}
+          />
+        </label>
+        <label>
+          <span>Assign to:</span>
+          <Select
+            onChange={(option) => setAssignedUsers(option)}
+            options={users}
+            isMulti
+          />
+        </label>
+        <button className="btn">Add Project</button>
 
-      {formError && <p className="error">{formError}</p>}
+        {formError && <p className="error">{formError}</p>}
       </form>
     </div>
-  )
+  );
 }
