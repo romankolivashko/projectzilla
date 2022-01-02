1a490d7f0b79f1f93fc6cf496a13f562896005c7
Roman Kolivashko <rkolivashko@gmail.com>
2022-01-02 15:04:06 -0800

    Added project commets listing output to project summary

--- src/pages/project/ProjectComments.js
+++ src/pages/project/ProjectComments.js
@@ -1,35 +1,54 @@
-import { useState} from "react"
-import { timestamp } from "../../firebase/config"
-import { useAuthContext } from "../../hooks/useAuthContext"
-import { useFirestore } from "../../hooks/useFirestore"
+import { useState } from "react";
+import Avatar from "../../components/Avatar";
+import { timestamp } from "../../firebase/config";
+import { useAuthContext } from "../../hooks/useAuthContext";
+import { useFirestore } from "../../hooks/useFirestore";
 
 export default function ProjectComments({ project }) {
-  const { updateDocument, response } = useFirestore('projects')
-  const [newComment, setNewComment] = useState('')
-  const { user } = useAuthContext()
+  const { updateDocument, response } = useFirestore("projects");
+  const [newComment, setNewComment] = useState("");
+  const { user } = useAuthContext();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     const commentToAdd = {
       displayName: user.displayName,
       photoURL: user.photoURL,
       content: newComment,
       createdAt: timestamp.fromDate(new Date()),
-      id: Math.random()
-    }
-   
+      id: Math.random(),
+    };
+
     await updateDocument(project.id, {
-      comments: [...project.comments, commentToAdd]
-    })
+      comments: [...project.comments, commentToAdd],
+    });
     if (!response.error) {
-      setNewComment('')
+      setNewComment("");
     }
-  }
-  
+  };
+
   return (
     <div className="project-comments">
       <h4>Project Comments</h4>
+
+      <ul>
+        {project.comments.length > 0 &&
+          project.comments.map((comment) => (
+            <li key={comment.id}>
+              <div className="comment-author">
+                <Avatar src={comment.photoURL} />
+                <p>{comment.displayName}</p>
+              </div>
+              <div className="comment-date">
+                <p>date goes here</p>
+              </div>
+              <div className="comment-content">
+                <p>{comment.content}</p>
+              </div>
+            </li>
+          ))}
+      </ul>
       <form className="add-comment" onSubmit={handleSubmit}>
         <label>
           <span>Add new comment:</span>
@@ -37,11 +56,10 @@ export default function ProjectComments({ project }) {
             required
             onChange={(e) => setNewComment(e.target.value)}
             value={newComment}
-          ></textarea> 
+          ></textarea>
         </label>
         <button className="btn">Add Comment</button>
       </form>
-      
     </div>
-  )
+  );
 }
